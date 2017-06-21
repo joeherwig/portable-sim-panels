3ce5acd828d414c339449ee26fc6883ae550c99a
joeherwig <joeherwig@users.noreply.github.com>
2017-06-21 18:41:36 +0200

    remove unneccesary files

--- start.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-Command -computer localhost -ScriptBlock {npm start}  -AsJob
-
-$webserver = {
-	cd simconnect	
-	invoke-expression -Command ./Webserver.ps1
-}
-Invoke-Command -computer localhost -ScriptBlock $webserver -AsJob
