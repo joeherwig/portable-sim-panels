8ec92873d51baafb4b0d028475e08c5291f0c5f4
joeherwig <joachim.herwig@gmx.net>
2018-10-24 01:03:28 +0200

    Added the echo-server to be able to test the gauges and assembled panels from browser-console without the need to run the sim. Still broken because i can't connect anymore to marcins server.

--- public/libs/portable-sim-panels.js
+++ public/libs/portable-sim-panels.js
@@ -1,6 +1,7 @@
 window.onload = setTimeout(function(){
-  var socket = new io.Socket();
-  socket.connect('ws://sim:8088');
+  var socket = io.connect('ws://sim:8088');
+  //var socket = new io.Socket();
+  //socket.connect('ws://sim:8088');
     socket.on('onUpdate', function (data) {
     //Update(data);
     var update = new CustomEvent(
@@ -13,4 +14,73 @@ window.onload = setTimeout(function(){
     );
     window.dispatchEvent(update);
   });
+
+
+  function connectToEchoServer() {
+    update = new WebSocket('ws://sim:1234');
+    update.onopen = function(evt) { onOpenEcho(evt) };
+    update.onclose = function(evt) { onCloseEcho(evt) };
+    update.onmessage = function(evt) { onMessageEcho(evt) };
+    update.onerror = function(evt) { onErrorEcho(evt) };
+  }
+
+  function onOpenEcho(evt) {
+    console.log("CONNECTED to echo server");
+    //doSend("WebSocket rocks");
+    console.log('You can test your Gauges by sending the desired JSON to the echo-server by executing the following (or a similar) command at the console:')
+    console.log('update.send(JSON.stringify({"AIRSPEED INDICATED":(Math.random()*250)+30, "SELECTED DME":2, "NAV DME:2":Math.floor(Math.random()*2000)/10}))')
+  }
+
+  function onCloseEcho(evt) {
+    console.log("DISCONNECTED from echo server");
+  }
+
+  function onMessageEcho(evt) {
+    var update = new CustomEvent(
+      "update",
+      {
+        detail: JSON.parse(evt.data),
+        bubbles: true,
+        cancelable: false
+      }
+    );
+    window.dispatchEvent(update);
+  }
+  connectToEchoServer();
+
+
+  function connectToSimconnectServer() {
+    Simconnect = new WebSocket('ws://sim:8088');
+    Simconnect.onopen = function(evt) { onOpen(evt) };
+    Simconnect.onclose = function(evt) { onClose(evt) };
+    Simconnect.onmessage = function(evt) { onMessage(evt) };
+    Simconnect.onerror = function(evt) { onError(evt) };
+  }
+
+  function onOpen(evt) {
+    console.log("CONNECTED to Simconnect");
+    //doSend("WebSocket rocks");
+    console.log('You can test your Gauges by sending the desired JSON to the echo-server by executing the following (or a similar) command at the console:')
+    console.log('update.send(JSON.stringify({"AIRSPEED INDICATED":(Math.random()*250)+30, "SELECTED DME":2, "NAV DME:2":Math.floor(Math.random()*2000)/10}))')
+  }
+
+  function onClose(evt) {
+    console.log("DISCONNECTED from Simconnect");
+  }
+
+  function onMessage(evt) {
+    var update = new CustomEvent(
+      "update",
+      {
+        detail: JSON.parse(evt.data),
+        bubbles: true,
+        cancelable: false
+      }
+    );
+
+    function onError(evt){}
+
+    window.dispatchEvent(update);
+  }
+  connectToSimconnectServer();
 }, 10);
--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const
 ,   aircraftVars = require('./public/libs/getAircraftVars')
 ,   server = require('http').createServer(app)
 ,   io = require('socket.io').listen(server)
-,   bodyParser = require("body-parser");
+,   bodyParser = require("body-parser")
+,   WebSocket = require('ws')
+,   wss = new WebSocket.Server({port: 1234});
 
 let myVars = []
-,   json ={};
+,   json ={}
+,   CLIENTS=[];
+
+
 
 server.listen(8080);
 console.log('Der Server läuft nun unter http://127.0.0.1:8080' + '/');
@@ -17,4 +22,18 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/public/index.html');
+});
+
+wss.on('connection', function connection(ws) {
+    console.log('Verbindung von Client');
+    CLIENTS.push(ws);
+    ws.on('message', function incoming(data) {
+      console.log(data)
+      console.log(CLIENTS.length)
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+       });
+   });
 });
