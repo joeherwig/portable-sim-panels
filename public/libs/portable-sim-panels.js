e4140aeca07b38bd618d0d16c9f4f3ac470f4796
joeherwig <joachim.herwig@gmx.net>
2018-10-30 21:17:46 +0100

    updated the script to fetch the hostname of the server to connect to his websockets. Intended to be run on the simulation computer.

--- public/libs/portable-sim-panels.js
+++ public/libs/portable-sim-panels.js
@@ -1,7 +1,8 @@
 window.onload = setTimeout(function(){
-  var socket = io.connect('ws://sim:8088');
+
+  /*var socket = io.connect('ws://localhost:8088');
   //var socket = new io.Socket();
-  //socket.connect('ws://sim:8088');
+    socket.connect('ws://sim:8088');
     socket.on('onUpdate', function (data) {
     //Update(data);
     var update = new CustomEvent(
@@ -14,10 +15,10 @@ window.onload = setTimeout(function(){
     );
     window.dispatchEvent(update);
   });
-
+  */
 
   function connectToEchoServer() {
-    update = new WebSocket('ws://sim:1234');
+    update = new WebSocket('ws://'+window.location.hostname+':1234');
     update.onopen = function(evt) { onOpenEcho(evt) };
     update.onclose = function(evt) { onCloseEcho(evt) };
     update.onmessage = function(evt) { onMessageEcho(evt) };
@@ -25,10 +26,10 @@ window.onload = setTimeout(function(){
   }
 
   function onOpenEcho(evt) {
-    console.log("CONNECTED to echo server");
+    console.log("CONNECTED to echo server :1234");
     //doSend("WebSocket rocks");
     console.log('You can test your Gauges by sending the desired JSON to the echo-server by executing the following (or a similar) command at the console:')
-    console.log('update.send(JSON.stringify({"AIRSPEED INDICATED":(Math.random()*250)+30, "SELECTED DME":2, "NAV DME:2":Math.floor(Math.random()*2000)/10}))')
+    console.log('update.send(JSON.stringify({"AIRSPEED INDICATED":(Math.random()*250)+30, "SELECTED DME":2, "NAV DME:2":Math.floor(Math.random()*2000)/10, "PLANE BANK DEGREES":(Math.random()*40-20),"PLANE PITCH DEGREES":(Math.random()*25-10)}))')
   }
 
   function onCloseEcho(evt) {
@@ -48,9 +49,8 @@ window.onload = setTimeout(function(){
   }
   connectToEchoServer();
 
-
   function connectToSimconnectServer() {
-    Simconnect = new WebSocket('ws://sim:8088');
+    Simconnect = new WebSocket('ws://'+window.location.hostname+':8088');
     Simconnect.onopen = function(evt) { onOpen(evt) };
     Simconnect.onclose = function(evt) { onClose(evt) };
     Simconnect.onmessage = function(evt) { onMessage(evt) };
@@ -58,10 +58,8 @@ window.onload = setTimeout(function(){
   }
 
   function onOpen(evt) {
-    console.log("CONNECTED to Simconnect");
-    //doSend("WebSocket rocks");
-    console.log('You can test your Gauges by sending the desired JSON to the echo-server by executing the following (or a similar) command at the console:')
-    console.log('update.send(JSON.stringify({"AIRSPEED INDICATED":(Math.random()*250)+30, "SELECTED DME":2, "NAV DME:2":Math.floor(Math.random()*2000)/10}))')
+    console.log("CONNECTED to Simconnect :8088");
+    //Simconnect.send("WebSimXMLCode:(>K:SOUND_TOGGLE)");
   }
 
   function onClose(evt) {
@@ -69,6 +67,7 @@ window.onload = setTimeout(function(){
   }
 
   function onMessage(evt) {
+    console.log(evt.data)
     var update = new CustomEvent(
       "update",
       {
